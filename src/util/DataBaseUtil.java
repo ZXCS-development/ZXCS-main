6b9199bdc0592398160a45229e85a9210a922438
1061269222 <42395314+1061269222@users.noreply.github.com>
2018-08-17 09:24:42 +0800

    退货明细板块 lxh 817924

--- src/util/DataBaseUtil.java
+++ src/util/DataBaseUtil.java
@@ -8,14 +8,14 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 
 /**
- * 数据库工具类 提供两个方法：
- * 	1、getConnection() 		返回连接对象
- * 	2、closeConnection(...) 	关闭数据库连接对象
+ * 鏁版嵁搴撳伐鍏风被 鎻愪緵涓や釜鏂规硶锛�
+ * 	1銆乬etConnection() 		杩斿洖杩炴帴瀵硅薄
+ * 	2銆乧loseConnection(...) 	鍏抽棴鏁版嵁搴撹繛鎺ュ璞�
  */
 public class DataBaseUtil {
   //String url="jdbc:oracle:thin:@localhost:1521:orcl";
-	//String url="jdbc:oracle:thin:@192.168.123.207:1521:orcl";
-	String url="jdbc:oracle:thin:@172.16.4.100:1521:orcl";
+	String url="jdbc:oracle:thin:@192.168.123.207:1521:orcl";
+	//String url="jdbc:oracle:thin:@172.16.4.100:1521:orcl";
 	String urlMysql="jdbc:mysql://pcshao.cn:9910/zxcs?useSSL=false&autoReconnect=true&characterEncoding=UTF-8";
 	private static String user = "zxcsadmin";
 	private static String password = "123";
@@ -32,7 +32,7 @@ public class DataBaseUtil {
 	public static void main(String[] args) {
 		System.out.println(new DataBaseUtil().getConnection());
 	}
-	//获得连接对象
+	//鑾峰緱杩炴帴瀵硅薄
 	public Connection getConnection(){
 		try{
 			return DriverManager.getConnection(url, user, password);
@@ -41,7 +41,7 @@ public class DataBaseUtil {
 		}
 		return null;
 	}
-	//关闭相关对象
+	//鍏抽棴鐩稿叧瀵硅薄
 	public void closeConnection(Connection conn, PreparedStatement pstat){
 		closeDBObject(conn);
 		closeDBObject(pstat);
